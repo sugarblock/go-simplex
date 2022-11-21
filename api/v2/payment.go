babdac8239cd03c093b044ef86b327a289d222a5
bugbuilder <nelson@bennu.cl>
2022-11-21 15:32:09 -0300

    feat: omitempty

--- api/v2/payment.go
+++ api/v2/payment.go
@@ -1,33 +1,33 @@
 package v2
 
 type PaymentRequest struct {
-	AccountDetail     AccountDetail `json:"account_details"`
-	TransactionDetail `json:"transaction_details"`
+	AccountDetail     *AccountDetail     `json:"account_details,omitempty"`
+	TransactionDetail *TransactionDetail `json:"transaction_details,omitempty"`
 }
 
 type PaymentResponse struct {
-	IsKycUpdateRequired *bool `json:"is_kyc_update_required"`
+	IsKycUpdateRequired *bool `json:"is_kyc_update_required,omitempty"`
 }
 
 type AccountDetail struct {
-	AppProviderId  string      `json:"app_provider_id"`
-	AppVersionId   string      `json:"app_version_id"`
-	AppEndUserId   string      `json:"app_end_user_id"`
-	AppInstallDate string      `json:"app_install_date"`
-	Email          string      `json:"email"`
-	Phone          string      `json:"phone"`
-	SignupLogin    SignupLogin `json:"signup_login"`
+	AppProviderId  *string      `json:"app_provider_id,omitempty"`
+	AppVersionId   *string      `json:"app_version_id,omitempty"`
+	AppEndUserId   *string      `json:"app_end_user_id,omitempty"`
+	AppInstallDate *string      `json:"app_install_date,omitempty"`
+	Email          *string      `json:"email,omitempty"`
+	Phone          *string      `json:"phone,omitempty"`
+	SignupLogin    *SignupLogin `json:"signup_login,omitempty"`
 }
 
 type SignupLogin struct {
-	Location           string `json:"location"`
-	Uaid               string `json:"uaid"`
-	AcceptLanguage     string `json:"accept_language"`
-	HttpAcceptLanguage string `json:"http_accept_language"`
-	UserAgent          string `json:"user_agent"`
-	CookieSessionId    string `json:"cookie_session_id"`
-	Timestamp          string `json:"timestamp"`
-	Ip                 string `json:"ip"`
+	Location           *string `json:"location,omitempty"`
+	Uaid               *string `json:"uaid,omitempty"`
+	AcceptLanguage     *string `json:"accept_language,omitempty"`
+	HttpAcceptLanguage *string `json:"http_accept_language,omitempty"`
+	UserAgent          *string `json:"user_agent,omitempty"`
+	CookieSessionId    *string `json:"cookie_session_id,omitempty"`
+	Timestamp          *string `json:"timestamp,omitempty"`
+	Ip                 *string `json:"ip,omitempty"`
 }
 
 type TransactionDetail struct {
